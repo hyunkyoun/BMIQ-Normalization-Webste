08a6b48ad37b86fbc557ce163fccc0354a08fbbd
abcd918 <eseo0229@gmail.com>
2025-01-20 20:43:47 -0500

    Update requirements.txt

--- backend/test.py
+++
@@ -1,7 +0,0 @@
-from script_connection import compute_results
-
-
-probe_data_path = './data/probesample.xlsx'
-beta_data_path = './data/beta.xlsx'
-
-compute_results(probe_data_path, beta_data_path)
